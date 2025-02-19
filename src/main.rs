9a0eb585c98469673a78fcff7b25376e27014946
agdev04 <dev@agnieve.com>
2025-02-19 08:44:29 +0800

    change configuration

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ async fn main() -> std::io::Result<()> {
             .configure(guard_config)
             .wrap(Logger::default())
     })
-    .bind(("127.0.0.1", 8000))?
+    .bind(("0.0.0.0", 8000))?
     .run()
     .await
 }
